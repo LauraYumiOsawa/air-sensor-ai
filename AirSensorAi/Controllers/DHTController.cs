136ec933c7f9c9e653a54fd06ad12121f1a3dd4e
Letícia Pontes <50830736+leticia-pontes@users.noreply.github.com>
2026-03-04 00:23:29 +0000

    Update project configuration and add .gitignore files
    - Updated project.nuget.cache to reflect new package paths and added warnings for compatibility issues with certain packages.
    - Added a global .gitignore file to exclude common build artifacts and sensitive configuration files.
    - Introduced a specific .gitignore file for the AirSensorAi project to manage ignored files effectively.

--- AirSensorAi/Controllers/DHTController.cs
+++ AirSensorAi/Controllers/DHTController.cs
@@ -1,21 +1,46 @@
+using FirebaseAdmin;
+using FirebaseAdmin.Database;
+using Google.Apis.Auth.OAuth2;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("api/[controller]")]
 public class DHTController : ControllerBase
 {
-    public static List<DeviceDto> DeviceDataList = new List<DeviceDto>();
+    private static FirebaseApp _firebaseApp;
+    private static FirebaseDatabase _database;
+
+    public DHTController()
+    {
+        if (_firebaseApp == null)
+        {
+            string jsonConfig = Environment.GetEnvironmentVariable("FIREBASE_CONFIG");
+
+            if (!string.IsNullOrEmpty(jsonConfig))
+            {
+                _firebaseApp = FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.FromJson(jsonConfig)
+                });
+
+                _database = FirebaseDatabase.GetInstance(_firebaseApp, "https://airsensorai-default-rtdb.firebaseio.com");
+            }
+        }
+    }
 
     [HttpPost]
-    public IActionResult Post([FromBody] DeviceDto deviceData)
+    public async Task<IActionResult> Post([FromBody] DeviceDto deviceData)
     {
-        DeviceDataList.Add(deviceData);
+        var dbRef = _database.GetReference("Leituras");
+        var newPostRef = dbRef.Push(); // Cria um ID único
+        await newPostRef.SetAsync(deviceData);
         return Ok(deviceData);
     }
 
     [HttpGet]
-    public IActionResult Get()
+    public async Task<IActionResult> Get()
     {
-        return Ok(DeviceDataList);
+        var snapshot = await _database.GetReference("Leituras").GetValueAsync();
+        return Ok(snapshot.Value);
     }
 }
